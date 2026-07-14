52082181d1614ccef3367fcea9dbe96037cd1a2b
Algorsky <dhy4568527913@163.com>
2026-07-14 17:56:58 +0800

    初始化

---
+++ miniRV_basic/src/rtl/ip/IROM/IROM_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+-- Date        : Tue Jul 14 15:37:07 2026
+-- Host        : LAPTOP-U64TB2N2 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               e:/Downloads/miniRV_basic_minisys/miniRV_basic/src/rtl/ip/IROM/IROM_stub.vhdl
+-- Design      : IROM
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tfgg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity IROM is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 13 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end IROM;
+
+architecture stub of IROM is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[13:0],douta[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_8,Vivado 2024.1";
+begin
+end;
